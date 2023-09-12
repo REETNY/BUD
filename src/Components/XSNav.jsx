c7923009cf98ec2a353bedd68fb1ae3bb6867f1e
REETNY <shamsideenajide@gmail.com>
2023-09-12 14:47:09 +0100

    XSMAX Header

--- src/Components/XSNav.jsx
+++ src/Components/XSNav.jsx
@@ -11,6 +11,8 @@ const XSNav = ({isOpenBool, func, userLogged}) => {
         borderBottom: "2px solid #ecb318"
     }
 
+    const userData = JSON.parse(localStorage.getItem("signedUser")) || [];
+
   return (
     <div className={isOpenBool ? `xmNav active` : `xmNav`}>
         <ul className="xmLinks">
@@ -31,13 +33,46 @@ const XSNav = ({isOpenBool, func, userLogged}) => {
             </li>}
 
             {userLogged && <li className="links">
-                <NavLink onClick={() => func()} style={({isActive}) => isActive ? activeStyles : {}} to="user" >User</NavLink>
+              <NavLink style={({isActive}) => isActive ? activeStyles : {}} to="user" >{userData.username}</NavLink>
             </li>}
 
             {userLogged && <li className="links" id='isUser' style={{position: "relative"}} >
-              <NavLink style={({isActive}) => isActive ? activeStyles : {}} to="settings" >
-                <FaUserAlt onClick={() => func()} style={{fontSize: "19px"}} />
-              </NavLink>
+                
+              {
+                userData.avatar == ""
+                ?
+                  <NavLink style={({isActive}) => isActive ? activeStyles : {}} to="settings" >
+                  <FaUserAlt style={{fontSize: "19px"}} />
+                  </NavLink>
+                :
+
+                <NavLink to="settings" style={({isActive}) => isActive ? activeStyles : {}}>
+                  <div className="userIcon" style={{width: "55px", height: "55px"}}>
+                    <div className="iconTab" style={{width: "100%", height: "100%", borderRadius: "50%", overflow: "hidden", position: "relative"}}>
+
+                      <span className="coloredItem">
+                        <div className="box"></div>
+                        <div className="box"></div>
+                        <div className="box"></div>
+                        <div className="box"></div>
+                      </span>
+
+                      <div className="iconCont" style={{width: "100%", height: "100%", overflow: "hidden",  display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "50%", background: "rgb(225, 225, 225, .3)", zIndex: "9999"}}>
+
+                        <div className="iconImg" style={{width: "85%", height: "85%", overflow: "hidden", zIndex: "999"}}>
+
+                          <img src={userData.avatar} alt="" style={{width: "100%", height: "100%", borderRadius: "50%", zIndex: "99"}} />
+
+                        </div>
+
+                      </div>
+
+                    </div>
+                  </div>
+                </NavLink>
+
+              }
+
             </li>}
         </ul>
     </div>
